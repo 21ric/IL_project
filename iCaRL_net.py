fb195f8176a8e08afcf5c42c7c04bdbddd57d2e9
alallala <laura.gabrieleft@gmail.com>
2020-05-22 12:27:09 +0200

    classify function

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -153,51 +153,9 @@ class iCaRL(nn.Module):
 
     self.exemplars.append(exemplar_set)
 
-  #da cambiare completamente
  
 
     def classify(self, dataloader):
        
-        #compute the mean for each examplars 
-        exemplar_means=[]
-        for exemplars in self.exemplars:
-            features = []
         
-            for ex in exemplars:
-            	features.append(self.feature_extractor.extract_features(ex))
-            exemplar_means.append(np.mean(features))
-        
-        if exemplar_means is None:
-            raise ValueError(
-                "Cannot classify without built examplar means,"
-            )
-        
-        if exemplar_means.shape[0] != self.num_classes:
-            raise ValueError(
-                "The number of examplar means ({}) is inconsistent".format(exemplar_means.shape[0])"
-            )
-
-        ypred = []
-        ytrue = []
-
-        for _, inputs, targets in dataloader:
-            inputs = inputs.to(DEVICE)
-
-            #compute the feature map of the input 
-            features = self.feature_extractor(inputs)
-            
-            pred_labels = []
-            
-            for feature in features:
-              #computing L2 distance
-              distances = torch.pow(examplar_mean - feature, 2).sum(-1)
-              pred_labels.append(distances.argmin().item())
-              
-            preds = np.array(pred_labels)
-
-            ypred.extend(preds)
-            ytrue.extend(targets)
-
-        return np.array(ypred), np.array(ytrue)
-
 
