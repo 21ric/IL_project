73608fbf78e925ae76f3eeafcb3a9657e191bd4f
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-23 00:12:04 +0200

    Update LWF_net.py

--- LWF_net.py
+++ LWF_net.py
@@ -10,6 +10,21 @@ from PIL import Image
 from resnet import resnet32
 
 import math
+import copy
+
+'''
+import torch
+torch.backends.cudnn.benchmark=True
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+import time
+import copy
+'''
 
 ####Hyper-parameters####
 LR = 2
@@ -69,20 +84,112 @@ class LwF(nn.Module):
 		out_features = self.fc.out_features
 		weight = self.fc.weight.data
 
-		if self.n_known == 0:
+		if self.n_known == 0: # First iteration
 			new_out_features = n
-		else:
+		else: # Other iterations
 			new_out_features = out_features + n
+			
 		print('new out features: ', new_out_features)
+		# Update model, changing last FC layer
 		self.model.fc = nn.Linear(in_features, new_out_features, bias=False)
+		# Update attribute self.fc
 		self.fc = self.model.fc
 		
+		# Initialize weights with kaiming normal
 		kaiming_normal_init(self.fc.weight)
+		# Upload old FC weights
 		self.fc.weight.data[:out_features] = weight
 		self.n_classes += n
     
 
 
+	def classify(self, images):
+		"""Classify images by softmax
+		Args:
+			x: input image batch
+		Returns:
+			preds: Tensor of size (batch_size,)
+		"""
+		_, preds = torch.max(torch.softmax(self.forward(images), dim=1), dim=1, keepdim=False)
+		return preds
+	
+	
+	
+	
+	def update(self, dataset, class_map, args):
+
+		self.compute_means = True
+
+		# Save a copy to compute distillation outputs
+		prev_model = copy.deepcopy(self)
+		prev_model.cuda()
+		
+		# Save true labels (new images)
+		classes = list(set(dataset.targets))
+		print("Classes: ", classes)
+		print('Known: ', self.n_known)
+		
+		'''
+		if self.n_classes == 1 and self.n_known == 0:
+			new_classes = [classes[i] for i in range(1,len(classes))]
+		else:
+			new_classes = [cl for cl in classes if class_map[cl] >= self.n_known]
+		'''
+		new_classes = classes #lista (non duplicati) con targets di train. len(classes)=10
+		
+		if len(new_classes) > 0:
+			self.increment_classes(new_classes)
+			self.cuda()
+
+		optimizer = self.optimizer
+		#optim.SGD(self.parameters(), lr=self.init_lr, momentum = self.momentum, weight_decay=self.weight_decay)
+
+		with tqdm(total=NUM_EPOCHS) as pbar:
+			for epoch in range(NUM_EPOCHS):
+
+				# Modify learning rate
+				# if (epoch+1) in lower_rate_epoch:
+				# 	self.lr = self.lr * 1.0/lr_dec_factor
+				# 	for param_group in optimizer.param_groups:
+				# 		param_group['lr'] = self.lr
+
+
+				for  images, labels, indices in dataset:
+					
+					seen_labels = []
+					
+					images = images.to(DEVICE)
+					labels = labels.to(DEVICE)
+					indices = indices.to(DEVICE)
+					#images = Variable(torch.FloatTensor(images)).cuda()
+					seen_labels = torch.LongTensor([class_map[label] for label in labels.numpy()])
+					#labels = Variable(seen_labels).cuda()
+					# indices = indices.cuda()
+
+					optimizer.zero_grad()
+					logits = self.forward(images)
+					cls_loss = nn.CrossEntropyLoss()(logits, labels)
+					if self.n_classes//len(new_classes) > 1:
+						dist_target = prev_model.forward(images)
+						logits_dist = logits[:,:-(self.n_classes-self.n_known)]
+						dist_loss = MultiClassCrossEntropy(logits_dist, dist_target, 2)
+						loss = dist_loss+cls_loss
+					else:
+						loss = cls_loss
+
+
+
+
+					loss.backward()
+					optimizer.step()
+
+					if (i+1) % 1 == 0:
+						tqdm.write('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
+							   %(epoch+1, self.num_epochs, i+1, np.ceil(len(dataset)/self.batch_size), loss.data))
+
+				pbar.update(1)
+
+"""
   def update_representation(self, dataset):
 
     targets = list(set(dataset.targets))
@@ -212,3 +319,4 @@ class LwF(nn.Module):
     pred = np.argmin(distances)
 
     return preds
+    """
