394d893e649b8d5994aae58b4c76afbad7741597
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-23 19:52:45 +0200

    m1

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -94,7 +94,7 @@ class iCaRL(nn.Module):
             out = self(images)
 
             #classification Loss
-            loss = sum(self.loss(out[:,y], 1 if y==labels else 0) for y in range(self.num_known, self.num_classe))
+            loss = sum(self.loss(out[:,y], 1 if y==labels else 0) for y in range(self.num_known, self.num_classes))
             #distillation Loss
             if self.num_known > 0:
                 #print('dist loss')
