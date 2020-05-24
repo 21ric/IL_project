e7c9033a1b89bd0a536162e0ed323a55a541ecbf
alallala <laura.gabrieleft@gmail.com>
2020-05-24 15:48:21 +0200

    update ft

--- fine_tuning.py
+++ fine_tuning.py
@@ -154,6 +154,7 @@ def main():
 #define images transformation
   train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
                                         transforms.RandomHorizontalFlip(),
+                                        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5)
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                         #transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
