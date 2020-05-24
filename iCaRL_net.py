c07fdffc139093ee70a088c64307f5e52f134f65
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-24 19:52:24 +0200

    m1

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -170,6 +170,8 @@ class iCaRL(nn.Module):
         #mu_p = mu_p / np.linalg.norm(mu_p)
         i = np.argmin(np.sqrt(np.sum((mu - mu_p) ** 2, axis=1)))
 
+        print('chosen i:{}'.formt(i))
+
         exemplar_set.append(images[i])
         exemplar_features.append(features[i])
         #print('Indice scelto:{}'.format(i))
