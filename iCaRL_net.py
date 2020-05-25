04e88410a81d8e80bf1fd2271db959301e94d34f
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-25 16:03:27 +0200

    m1

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -24,10 +24,13 @@ NUM_EPOCHS = 4
 DEVICE = 'cuda'
 ########################
 def to_onehot(targets, batch_size, num_classes):
-    targets_onehot = torch.FloatTensor(batch_size, num_classes)
-    targets_onehot.zero_()
-    targets_onehot.scatter_(1, targets, 1)
-    return targets_onehot
+    one_hots = np.array()
+    for t in targets:
+        temp = np.zeros(num_classes)
+        temp[t] = 1
+        onehots.append(temp)
+    return one_hots
+
 
 class iCaRL(nn.Module):
     def __init__(self, n_classes):
