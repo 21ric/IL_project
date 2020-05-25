cfe03c50548085838d55d1238e0647d6b1da2cbe
alallala <laura.gabrieleft@gmail.com>
2020-05-25 23:52:54 +0200

    update LwF_net

--- LWF.py
+++ LWF.py
@@ -1,7 +1,7 @@
 from LWF_net import LwF
 
 from torchvision import transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import Subset, DataLoader
 from torch.autograd import Variable
 from sklearn.model_selection import train_test_split
 
