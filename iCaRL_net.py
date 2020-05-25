1b8e9b3e962bc8315d93bc4ec744211d06e341cc
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-25 16:21:57 +0200

    m1

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -105,7 +105,8 @@ class iCaRL(nn.Module):
             i = 0
             for imgs, labels, indexes in loader:
                 imgs = imgs.to(DEVICE)
-                labels = to_onehot(targets, BATCH_SIZE, self.n_classes)
+
+                labels = to_onehot(labels, BATCH_SIZE, self.n_classes)
                 labels = labels.to(DEVICE)
                 indexes = indexes.to(DEVICE)
 
