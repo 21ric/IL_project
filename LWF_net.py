1d7fe076e267c70a4e3f6868c165a067e82f260a
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-26 18:44:51 +0200

    Update LWF_net.py

--- LWF_net.py
+++ LWF_net.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torchvision import transforms
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import numpy as np
