df7c2c8a2569e5dea9d7251c60e44a8054dfe6e5
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-26 15:45:07 +0200

    m1

--- iCaRL_net2.py
+++ iCaRL_net2.py
@@ -10,6 +10,8 @@ from PIL import Image
 
 from resnet import resnet32
 
+import copy
+
 import math
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
