8cab15ea0792688988454826cdb990c69e5eb7a6
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-26 18:29:48 +0200

    m1

--- iCaRL2.py
+++ iCaRL2.py
@@ -28,7 +28,7 @@ def main():
     #define images transformation
     #define images transformation
     train_transform = transforms.Compose([transforms.RandomCrop(32, padding=4),
-                                        transforms.RandomHorizontalFlip(),
+                                        #transforms.RandomHorizontalFlip(),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                         #transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
@@ -91,6 +91,7 @@ def main():
 
         val_dataset.dataset.transform = test_transform
 
+        print('-'*30)
         print('Updating representation ...')
         print('-'*30)
 
