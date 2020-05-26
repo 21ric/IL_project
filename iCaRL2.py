6950aeb5d7547e35c926ef2f077324f9a01a2aa6
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-26 02:09:23 +0200

    Update iCaRL2.py

--- iCaRL2.py
+++ iCaRL2.py
@@ -51,6 +51,11 @@ def main():
     classes_groups = np.array_split(all_classes, 10)
     print(classes_groups)
 
+    for i in range(10):
+        for list in classes_groups[:i]:
+            for y in list:
+                print(y)
+            print("fine lista")
     #num_iters = total_classes//CLASSES_BATCH      
     
     # Create class map
