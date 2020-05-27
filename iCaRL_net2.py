5d47060e4e10969aee423f49a8d46634c3161591
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-27 20:32:32 +0200

    m1

--- iCaRL_net2.py
+++ iCaRL_net2.py
@@ -337,7 +337,7 @@ class iCaRL(nn.Module):
         #assert self._means.shape[0] == self._n_classes
 
         for exemplars in self.exemplar_sets:
-            features = self.features_extractor.extract_features(exemplars)
+            features = self.features_extractor.extract_features(torch.FloatTensor(exemplars))
             features = self._l2_normalize(features)
             self.exemplars_means.append(self._l2_normalize(torch.mean(features)))
 
