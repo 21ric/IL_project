f0cf9a33553ca860f693db90f13b521831547e29
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-28 02:47:39 +0200

    Update iCaRL_net2.py

--- iCaRL_net2.py
+++ iCaRL_net2.py
@@ -14,9 +14,9 @@ import copy
 
 import math
 
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))])
-#transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+#transform = transforms.Compose([transforms.ToTensor(),
+#                               transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))])
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
 ####Hyper-parameters####
 LR = 2
