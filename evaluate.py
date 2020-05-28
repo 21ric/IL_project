6b239335a49801aecb37d43236a7b550f0bfe810
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-28 20:18:35 +0200

    Create evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,27 @@
+import fine_tuning as ft
+import LwF2 as lwf
+import iCaRL2 as icarl
+
+import utils
+
+
+def main():
+    print(basta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
