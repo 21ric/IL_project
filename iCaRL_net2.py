f8481828893a4cd7e885f73bd878590b1a171686
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-28 04:49:18 +0200

    Update iCaRL_net2.py

--- iCaRL_net2.py
+++ iCaRL_net2.py
@@ -124,7 +124,7 @@ class iCaRL(nn.Module):
 
         #self.n_classes += n
         #optimizer = optim.SGD(self.features_extractor.parameters(), lr=2.0, weight_decay=0.00001, momentum=0.9)
-        optimizer = optim.Adam(self.features_extractor.parameters(), lr=0.02, weight_decay=0.00001)
+        optimizer = optim.Adam(self.features_extractor.parameters(), lr=0.002, weight_decay=0.00001)
 
         i = 0
 
