56a20126bee49d44c42074d3b522b6bf569b5b85
alallala <laura.gabrieleft@gmail.com>
2020-05-29 12:08:32 +0200

    eval

--- evaluate.py
+++ evaluate.py
@@ -58,7 +58,7 @@ def main():
     # Plot it !
     plt.figure(figsize=(8, 6))
     for key in dict_1:
-       plt.plot([i for i in range(10,110,10)],dict_1[key],marker = 'o', markersize=7, label=key)
+       plt.plot([i for i in range(10,110)],dict_1[key],marker = 'o', markersize=7, label=key)
 
     #plt.style.use('seaborn-whitegrid')
     plt.title('Results for classes group: 1')
