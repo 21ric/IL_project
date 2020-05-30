72f4c5a933ddb407464f6cc35888903461b8abeb
alallala <laura.gabrieleft@gmail.com>
2020-05-31 01:30:37 +0200

    iCaRL_net

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -26,7 +26,7 @@ transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4
 #transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 ####Hyper-parameters####
-LR = 2
+LR = 0.01
 WEIGHT_DECAY = 0.00001
 BATCH_SIZE = 128
 STEPDOWN_EPOCHS = [49, 63]
