5983015efe1c899fa07cc86b5389ea2607f3d3a7
alallala <laura.gabrieleft@gmail.com>
2020-05-31 14:26:48 +0200

    iCaRL_net

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -177,7 +177,7 @@ class iCaRL(nn.Module):
                 optimizer.zero_grad()
                 out = self(imgs)
 
-
+                '''
                 if self.loss_config == 0:
                     #BCE
                     loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
@@ -196,7 +196,7 @@ class iCaRL(nn.Module):
                     #MSE
                     loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
 
-                '''
+                
                 if self.n_known > 0:
 
                     q_i = q[indexes]
@@ -255,7 +255,7 @@ class iCaRL(nn.Module):
                    target = torch.cat((q_i[:,:self.n_known],labels_hot[:,self.n_known:self.n_classes]),dim=1)
                    dist_loss = self.dist_loss(out,target)
 
-                   loss = (1/(iter+1))*loss + (iter/(iter+1))*dist_loss
+                   #loss = (1/(iter+1))*loss + (iter/(iter+1))*dist_loss
 
                 loss.backward()
                 optimizer.step()
