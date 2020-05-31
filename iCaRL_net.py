b110ecb8b698fde7564729d36702ffbac3a47664
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-31 22:31:48 +0200

    m10

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -147,57 +147,12 @@ class iCaRL(nn.Module):
                     out = torch.sigmoid(out)
 
                 loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
-                """
-
-                if loss config
-
-
-                if self.loss_config == 0:
-                    #BCE
-                    loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
-
-                elif self.loss_config == 1:
-                    #MLSML
-                    #out = torch.log(torch.softmax(out, dim=1))
-                    loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
-
-                elif self.loss_config == 2:
-                    #L1
-                    out = torch.sigmoid(out)
-                    loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
-
-                elif self.loss_config == 3:
-                    #MSE
-
-                    loss = self.clf_loss(torch.softmax(out[:, self.n_known:self.n_classes],dim=1), labels_hot[:, self.n_known:self.n_classes])
-                """
-
-
 
                 if self.n_known > 0:
 
                     q_i = q[indexes]
-
                     dist_loss = self.dist_loss(out[:, :self.n_known], q_i[:, :self.n_known])
 
-                    """
-                    if self.loss_config == 0:
-                        #BCE
-                        dist_loss = self.dist_loss(out[:, :self.n_known], q_i[:, :self.n_known])
-
-                    elif self.loss_config == 1:
-                        #NLLL
-                        dist_loss = self.dist_loss(out[:, :self.n_known], q_i[:, :self.n_known])
-
-                    elif self.loss_config == 2:
-                        #L1
-                        dist_loss = self.dist_loss(out[:, :self.n_known], q_i[:, :self.n_known])
-
-                    elif self.loss_config == 3:
-                        #MSE
-
-                        dist_loss = self.dist_loss(torch.softmax(out[:, :self.n_known],dim=1), q_i[:, :self.n_known])
-                    """
                     loss = (1/(iter+1))*loss + (iter/(iter+1))*dist_loss
 
                 loss.backward()
