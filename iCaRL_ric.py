33ae16aeef9de5b70af05b796b39d03182cae222
21ric <55327282+21ric@users.noreply.github.com>
2020-06-03 17:58:49 +0200

    Update iCaRL_ric.py

--- iCaRL_ric.py
+++ iCaRL_ric.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import transforms
 from torch.autograd import Variable
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 
 import numpy as np
 
@@ -58,9 +58,55 @@ class iCaRL(nn.Module):
 
     def forward(self, x):
         x = self.features_extractor(x)
-        return x
-    
-    def train(self, loader):
+        return x    
+
+    def add_classes(self, n):
+        in_features = self.features_extractor.fc.in_features
+        out_features = self.features_extractor.fc.out_features
+
+        weight = copy.deepcopy(self.features_extractor.fc.weight.data)
+        bias = copy.deepcopy(self.features_extractor.fc.bias.data)
+
+        self.features_extractor.fc = nn.Linear(in_features, out_features+n)
+        self.features_extractor.fc.weight.data[:out_features] = copy.deepcopy(weight)
+        self.features_extractor.fc.bias.data[:out_features] = copy.deepcopy(bias)
+
+        self.n_classes += n
+
+    def add_exemplars(self, dataset, map_reverse):
+        for y, exemplars in enumerate(self.exemplar_sets):
+            dataset.append(exemplars, [map_reverse[y]]*len(exemplars))
+
+    def update_representation(self, dataset, class_map, map_reverse, iter):
+
+        targets = list(set(dataset.targets))
+        n = len(targets)
+
+        print('New classes:{}'.format(n))
+        print('-'*30)
+
+        self.add_exemplars(dataset, map_reverse)
+
+        print('Datset extended to {} elements'.format(len(dataset)))
+
+        loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+
+        self.features_extractor.to(DEVICE)
+
+        f_ex = copy.deepcopy(self.features_extractor)
+        f_ex.to(DEVICE)
+        #compute previous output for training
+        q = torch.zeros(len(dataset), self.n_classes).to(DEVICE)
+        for images, labels, indexes in loader:
+            f_ex.train(False)
+            images = Variable(images).to(DEVICE)
+            indexes = indexes.to(DEVICE)
+            g = torch.sigmoid(f_ex.forward(images))
+            q[indexes] = g.data
+        q = Variable(q).to(DEVICE)
+
+        self.add_classes(n)
+        
         self.features_extractor.train(True)
         optimizer = optim.SGD(self.features_extractor.parameters(), lr=self.lr, weight_decay=WEIGHT_DECAY, momentum=MOMENTUM)
 
@@ -111,76 +157,16 @@ class iCaRL(nn.Module):
                   print('Learning rate:{}'.format(param_group['lr']))
                 print('-'*30)
             i+=1
-
-    def add_classes(self, n):
-        in_features = self.features_extractor.fc.in_features
-        out_features = self.features_extractor.fc.out_features
-
-        weight = copy.deepcopy(self.features_extractor.fc.weight.data)
-        bias = copy.deepcopy(self.features_extractor.fc.bias.data)
-
-        self.features_extractor.fc = nn.Linear(in_features, out_features+n)
-        self.features_extractor.fc.weight.data[:out_features] = copy.deepcopy(weight)
-        self.features_extractor.fc.bias.data[:out_features] = copy.deepcopy(bias)
-
-        self.n_classes += n
-
-    def add_exemplars(self, dataset, map_reverse):
-        for y, exemplars in enumerate(self.exemplar_sets):
-            dataset.append(exemplars, [map_reverse[y]]*len(exemplars))
-
-    def update_representation(self, dataset, class_map, map_reverse, iter):
-
-        targets = list(set(dataset.targets))
-        n = len(targets)
-
-        print('New classes:{}'.format(n))
-        print('-'*30)
-
-        #self.add_exemplars(dataset, map_reverse)
-
-        print('Datset extended to {} elements'.format(len(dataset)))
-
-        loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-
-        self.features_extractor.to(DEVICE)
-
-        f_ex = copy.deepcopy(self.features_extractor)
-        f_ex.to(DEVICE)
-        #compute previous output for training
-        q = torch.zeros(len(dataset), self.n_classes).to(DEVICE)
-        for images, labels, indexes in loader:
-            f_ex.train(False)
-            images = Variable(images).to(DEVICE)
-            indexes = indexes.to(DEVICE)
-            g = torch.sigmoid(f_ex.forward(images))
-            q[indexes] = g.data
-        q = Variable(q).to(DEVICE)
-
-        self.add_classes(n)
-        
-        self.train(loader)
         
 
     def reduce_exemplars_set(self, m):
         for y, exemplars in enumerate(self.exemplar_sets):
             self.exemplar_sets[y] = exemplars[:m]
-
-
+    
     @torch.no_grad()
-    def construct_exemplars_set(self, images, m, random_flag=False):
-
-        if random:
-            exemplar_set = []
-            indexes = random.sample(range(len(images)), m)
-            for i in indexes:
-                exemplar_set.append(images[i])
-            self.exemplar_sets.append(exemplar_set)
-
-        else:
-            features = []
-
-            self.features_extractor.to(DEVICE)
+    def compute_new_means(self, images):
+        
+        self.features_extractor.to(DEVICE)
 
 
             self.features_extractor.train(False)
@@ -192,9 +178,26 @@ class iCaRL(nn.Module):
 
             class_mean = np.mean(features, axis=0)
             class_mean = class_mean / np.linalg.norm(class_mean)
-
+            
             self.new_means.append(class_mean)
+        
 
+
+    @torch.no_grad()
+    def construct_exemplars_set(self, images, m, random_flag=False):
+        
+        features = []
+
+         self.compute_new_means()
+
+        if random:
+            exemplar_set = []
+            indexes = random.sample(range(len(images)), m)
+            for i in indexes:
+                exemplar_set.append(images[i])
+            self.exemplar_sets.append(exemplar_set)
+
+        else:
             exemplar_set = []
             exemplar_features = []
             for k in range(m):
@@ -207,7 +210,6 @@ class iCaRL(nn.Module):
 
                 exemplar_set.append(images[i])
                 exemplar_features.append(features[i])
-
                 if i == 0:
                     images = images[1:]
                     features = features[1:]
@@ -225,7 +227,58 @@ class iCaRL(nn.Module):
 
             self.exemplar_sets.append(np.array(exemplar_set))
             self.features_extractor.train(True)
+    
+    def train_on_exemplar():
+        exemplars = []
+        labels = []
+        for y, exemplars in (self.exemplar_sets):
+            exemplars.append(np.array(exemplars))
+            labels.append(np.array([y]*len(exemplars))
+                          
+        exemplars = torch.Tensor(exemplars) # transform to torch tensor
+        labels = torch.Tensor(labels)
 
+        dataset = TensorDataset(exemplars,labels) # create your datset
+        loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+        
+        
+        self.features_extractor.train(True)
+        optimizer = optim.SGD(self.features_extractor.parameters(), lr=self.lr, weight_decay=WEIGHT_DECAY, momentum=MOMENTUM)
+
+        i = 0
+
+        self.features_extractor.to(DEVICE)
+        for epoch in range(NUM_EPOCHS):
+
+            if epoch in STEPDOWN_EPOCHS:
+              for param_group in optimizer.param_groups:
+                param_group['lr'] = param_group['lr']/STEPDOWN_FACTOR
+
+
+            self.features_extractor.train(True)
+            for imgs, labels in loader:
+                imgs = imgs.to(DEVICE)
+                seen_labels = torch.LongTensor([class_map[label] for label in labels.numpy()])
+                labels = Variable(seen_labels).to(DEVICE)
+                labels_hot=torch.eye(self.n_classes)[labels]
+                labels_hot = labels_hot.to(DEVICE)
+
+
+                optimizer.zero_grad()
+                out = self(imgs)
+
+                loss = self.clf_loss(out[:, self.n_known:self.n_classes], labels_hot[:, self.n_known:self.n_classes])
+
+                loss.backward()
+                optimizer.step()
+
+
+            if i % 10 == 0 or i == (NUM_EPOCHS-1):
+                print('Epoch {} Loss:{:.4f}'.format(i, loss.item()))
+                for param_group in optimizer.param_groups:
+                  print('Learning rate:{}'.format(param_group['lr']))
+                print('-'*30)
+            i+=1
 
     @torch.no_grad()
     def classify(self, x, classifier):
