ef0394a358f207ee50f6a18497f2dbfd3d9fa894
21ric <55327282+21ric@users.noreply.github.com>
2020-06-03 19:58:14 +0200

    Update iCaRL_net.py

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -287,7 +287,7 @@ class iCaRL(nn.Module):
                 for mean in exemplar_means:
 
                     if classifier =='nme':
-                        measures.append((feat - mean).pow(2).sum().squeeze().item().cpu().numpy())
+                        measures.append((feat - mean).pow(2).sum().squeeze().item())
                     elif classifier =='nme-cosine':
                         measures.append(cosine_similarity(feat.unsqueeze(0).cpu().numpy(), mean.unsqueeze(0).cpu().numpy()))
 
