80c0dd86afdd8eb4c06abc740133900452460e75
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-06-04 14:38:17 +0200

    Update iCaRL_ric.py

--- iCaRL_ric.py
+++ iCaRL_ric.py
@@ -332,7 +332,7 @@ class iCaRL(nn.Module):
 
                 self.exemplar_means = exemplar_means
                 new_means = self.new_means #convert to CPU?
-                new_means = Variable(new_means).cpu()
+                new_means = new_means.cpu()
                 self.exemplar_means.extend(new_means)
                 print('lunghezza nuove medie', len(self.new_means))
                 self.compute_means = False
