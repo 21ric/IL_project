4725e2e9ca4715b286ddedb7e3b975e3b075352e
alallala <laura.gabrieleft@gmail.com>
2020-06-10 22:36:54 +0200

    iCaRL_alallala

--- iCaRL_net_alallala.py
+++ iCaRL_net_alallala.py
@@ -393,10 +393,10 @@ class iCaRL(nn.Module):
 
           labels_set = set([labels for _, labels, _ in test_dataloader])       #collecting all distinct labels of old images
           labels_set = list(labels_set)
-          print("len(labels_set)",labels_set)
+          print("len(labels_set)",len(labels_set))
           for i in range(10,len(test_dataloader.dataset)//100,10):                   
                   groups[i] = [[labels_set[i-10:i]],0]        #storing labels of group i and running corrects for that group 
-                  print(groups[i])                                           #groups[10] = [[0,1,2,3,4,5,6,7,8,9,10],running_corrects]   
+                  print('groups[i]',groups[i])                       #groups[10] [[0,1,2,3,4,5,6,7,8,9,10],running_corrects]   
                   acc_per_group[i] = 0                       #storing accuracy per group 
           print(groups,acc_per_group)
           running_corrects = 0                                              
