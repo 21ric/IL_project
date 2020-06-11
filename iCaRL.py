f63b3e66bc0173184b8001b44708c1dcb13545d3
21ric <55327282+21ric@users.noreply.github.com>
2020-06-11 15:40:20 +0200

    Update iCaRL.py

--- iCaRL.py
+++ iCaRL.py
@@ -59,10 +59,9 @@ def incremental_learning(dict_num,loss_config,classifier,lr, new_herding=False, 
         train_dataset, test_dataset = utils.get_train_test(classes_groups[i])
         
         if undersample:
-            train_indices, _ = train_test_split(range(len(train_dataset)), test_size=(1-resize_factor), stratify=train_dataset.targets)
-            train_dataset = Subset(train_dataset, train_indices)
+            train_dataset.reasample(resize_factor)
             print('len1', len(train_dataset))
-            #train_dataset = train_dataset.dataset
+            
             print('len2', len(train_dataset))
 
         print('-'*30)
