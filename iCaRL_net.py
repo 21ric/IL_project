fbe40c92a1d87d9a79008c212228185cfb3dfe35
21ric <55327282+21ric@users.noreply.github.com>
2020-06-13 15:52:54 +0200

    Update iCaRL_net.py

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -219,14 +219,14 @@ class iCaRL(nn.Module):
                         sample_out = out[~(labels >= self.n_known)]
                     
                         labels_ex = labels_hot[~(labels < self.n_known)]
-                        labels_sample = [~(labels >= self.n_known)]
+                        labels_sample = labels_hot[~(labels >= self.n_known)]
                         
                         #coeff_new, coeff_old = get_balanced_coefficients(0.8, card_new=500,num_new_classes=(self.n_classes-self.n_known),num_old_classes=self.n_known, i=iter, card_old=self.exemplars_per_class)
                         
                         coeff_old, coeff_new = 1, 1
                         
                         loss_ex = coeff_old * bce_sum(ex_out[:, self.n_known:], labels_ex[:, self.n_known:])
-                        loss_sample = coeff_new * bce_sum(ex_out[:, self.n_known:], labels_ex[:, self.n_known:])
+                        loss_sample = coeff_new * bce_sum(ex_out[:, self.n_known:], labels_sample[:, self.n_known:])
                         
                         loss = (loss_ex + loss_sample)/(len(out)*10)
                         
