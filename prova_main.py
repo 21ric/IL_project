dbffe34834aa1232d7d7824a956c12a4781dc31a
21ric <riccardoluigi.trinchieri@gmail.com>
2020-06-20 19:37:53 +0200

    a

--- prova_main.py
+++ prova_main.py
@@ -67,7 +67,7 @@ def incremental_learning(dict_num, loss_config, classifier, lr, loss1=False, und
         print('-'*30)
         
         for y in classes_groups[i]:
-           net.construct_exemplars_set(train_dataset.get_class_imgs(y), m*2, random_flag)
+           net.construct_exemplars_set(train_dataset.get_class_imgs(y), m, random_flag)
         
         
         
