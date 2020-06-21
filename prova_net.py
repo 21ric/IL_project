3ba65c92fdb8ea57fa36416dd3f639fd48b2b82e
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-06-21 18:01:32 +0200

    Update prova_net.py

--- prova_net.py
+++ prova_net.py
@@ -323,7 +323,7 @@ class iCaRL(nn.Module):
         f_prev_net.to(DEVICE)
         
         self.features_extractor.train(True)
-        optimizer = optim.SGD(self.features_extractor.parameters(), lr=2, weight_decay=WEIGHT_DECAY, momentum=MOMENTUM)
+        optimizer = optim.SGD(self.features_extractor.parameters(), lr=1, weight_decay=WEIGHT_DECAY, momentum=MOMENTUM)
         i = 0
         self.features_extractor.to(DEVICE)
         
