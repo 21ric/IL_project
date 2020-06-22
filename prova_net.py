66410d3bede4ba327cc9fdb8d7b9ea7802edf7bc
21ric <riccardoluigi.trinchieri@gmail.com>
2020-06-22 18:52:50 +0200

    a

--- prova_net.py
+++ prova_net.py
@@ -219,13 +219,13 @@ class iCaRL(nn.Module):
                     #for _ in range(50):
                     for j in range(len(exemplars)-1):
                         
-                            #i1, i2 = np.random.randint(0, len(exemplars)), np.random.randint(0, len(exemplars))
+                            i1, i2 = np.random.randint(0, len(exemplars)), np.random.randint(0, len(exemplars))
                             
                             w=0.4
                             
-                            new_point = w*exemplars[j] + (1-w)*exemplars[j+1]
-                            new_target = w*ex_labels[j] + (1-w)*ex_labels[j+1]
-                            new_q_i = w*q_i_ex[j] + (1-w)*q_i_ex[j+1]
+                            new_point = w*exemplars[i1] + (1-w)*exemplars[i2]
+                            new_target = w*ex_labels[i1] + (1-w)*ex_labels[i2]
+                            new_q_i = w*q_i_ex[i1] + (1-w)*q_i_ex[i2]
 
                             mix_up_points.append(new_point)
                             mix_up_targets.append(new_target)
