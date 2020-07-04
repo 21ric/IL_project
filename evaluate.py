8ec955c4b0e96ae012a04c2df58496e4f42361ab
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-07-04 15:25:16 +0200

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -8,28 +8,14 @@
 
 import fine_tuning as ft
 import LwF as lwf
-import iCaRL as icarl
+import iCaRL_main as icarl
 
 import matplotlib.pyplot as plt
 
 import utils
 
 
-'''
-def plot_accuracy(classes_split,num):
 
-    plt.figure(figsize=(8, 6))
-    for key in classes_split:
-       plt.plot([i for i in range(10,110,10)],classes_split[key],marker = 'o', markersize=7, label=key)
-
-    plt.legend()
-    plt.title("Test accuracy with split: %s" %str(num))
-    plt.xlabel('Known classes')
-    plt.ylabel('Accuracy')
-    plt.show()
-
-    return
-'''
 
 def main(i):   # The parameter i can be set to ['1','2','3'] depending on the random split of the dataset you want to load.
                # Change this value if you want to perform calculations with other random splits
@@ -46,13 +32,7 @@ def main(i):   # The parameter i can be set to ['1','2','3'] depending on the ra
         
         # Call the incremental_learning function to perform train+test on 10 iterations
         if learner == icarl:
-            # iCaRL's incremental learning function has additional parameters: 
-            # loss_config = ['bce', 'mse', 'l1', 'kl']  is the distillation loss function we want to use. 
-            #                                           classification loss is always bce.
-            # classifier = ['nme', 'knn', 'svc']        is the classifier we want to use.
-            # lr = int                                  is the learning rate.
-            # new_herding = [True, False]               is automatically set to False. If new_herding = True, the custom 
-            #                                           exemplar selection will be performed.
+
             new_acc_, old_acc_, all_acc_ = learner.incremental_learning(i, loss_config='bce', classifier='nme',lr=2.0)
         
         else:
